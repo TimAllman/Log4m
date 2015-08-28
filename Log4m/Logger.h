907aa89caffd9e6c4e2a876472ba929c82036c49
Tim Allman <dr.tim.allman@gmail.com>
2015-08-28 11:46:36 -0400

    Improved doxygen comments.

--- Log4m/Logger.h
+++ Log4m/Logger.h
@@ -2,9 +2,23 @@
 //  Logger.h
 //  log4m
 //
-//  Created by Tim Allman on 2013-04-28.
-//  Copyright (c) 2013 Tim Allman. All rights reserved.
-//
+
+/* Log4m is an Obj-C front end for the C++ logger log4cplus
+ * (http://sourceforge.net/p/log4cplus/wiki/Home/ ).
+ * Copyright (C) 2014  Tim Allman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <Foundation/Foundation.h>
 
